abb180ac25930e6b33805ba0fc6acdffe1039d34
josephPol <josephpoljak1@gmail.com>
2026-04-30 16:31:22 +0200

    Initial commit

---
+++ src/main/java/org/example/reportsworskhopgft/ReportsWorskhopGftApplication.java
@@ -0,0 +1,13 @@
+package org.example.reportsworskhopgft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportsWorskhopGftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportsWorskhopGftApplication.class, args);
+    }
+
+}
