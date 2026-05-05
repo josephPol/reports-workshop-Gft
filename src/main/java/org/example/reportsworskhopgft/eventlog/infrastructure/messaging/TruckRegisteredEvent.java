8480d14528eb7a51753541a7ac6f2453b111729c
Jaime <jmgo@gft.com>
2026-05-05 16:59:43 +0200

    [reporting] feat(consumer): add transport event consumer

---
+++ src/main/java/org/example/reportsworskhopgft/eventlog/infrastructure/messaging/TruckRegisteredEvent.java
@@ -0,0 +1,13 @@
+package org.example.reportsworskhopgft.eventlog.infrastructure.messaging;
+
+import java.util.UUID;
+
+public record TruckRegisteredEvent(
+        UUID truckId,
+        String name,
+        Position position,
+        int capacity,
+        int timestamp
+) {
+    public record Position(int x, int y) {}
+}
