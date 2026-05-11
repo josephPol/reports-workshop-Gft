cf44cd1a6402448af51649b5081352ad83add467
jrtu <jrtu@gft.com>
2026-05-11 15:37:07 +0200

    [reporting] refactor(eventlog): decouple domain model and resolve bean conflicts

--- src/main/java/org/example/reportsworskhopgft/blockedorder/infrastructure/BlockedOrderRepositoryJPA.java
+++ src/main/java/org/example/reportsworskhopgft/blockedorder/infrastructure/BlockedOrderRepositoryJPA.java
@@ -1,6 +1,6 @@
 package org.example.reportsworskhopgft.blockedorder.infrastructure;
 
-import org.example.reportsworskhopgft.blockedorder.infrastructure.persistance.BlockedOrderJPA;
+import org.example.reportsworskhopgft.blockedorder.infrastructure.persistence.BlockedOrderJPA;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
