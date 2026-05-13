ae818ca6cd04603d8be64f3f388dd8599602fe67
josephPol <josephpoljak1@gmail.com>
2026-05-13 15:53:32 +0200

    Merge remote-tracking branch 'origin/master'

--- src/test/java/org/example/reportsworskhopgft/ReportsWorskhopGftApplicationMainTest.java
+++ src/test/java/org/example/reportsworskhopgft/ReportsWorskhopGftApplicationMainTest.java
@@ -11,25 +11,23 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
         webEnvironment = SpringBootTest.WebEnvironment.NONE,
         useMainMethod = SpringBootTest.UseMainMethod.ALWAYS,
         args = {
-            "--spring.main.web-application-type=none",
-            "--spring.main.banner-mode=off",
-            "--spring.jmx.enabled=false",
-            "--management.endpoints.enabled-by-default=false",
-            "--spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,"
-                    + "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,"
-                    + "org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration,"
-                    + "org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration"
+                "--spring.main.web-application-type=none",
+                "--spring.main.banner-mode=off",
+                "--spring.jmx.enabled=false",
+                "--management.endpoints.enabled-by-default=false",
+                "--spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,"
+                        + "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,"
+                        + "org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration,"
+                        + "org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration"
         })
 class ReportsWorskhopGftApplicationMainTest {
 
     @MockitoBean private EventLogRepositoryJPA eventLogRepositoryJPA;
-
     @MockitoBean private BlockedOrderRepositoryJPA blockedOrderRepositoryJPA;
-
     @MockitoBean private ConnectionFactory connectionFactory;
 
     @Test
     void main_should_run() {
-        // Intentionally empty: context bootstrap runs through the app's main method.
+        ReportsWorskhopGftApplication.main(new String[]{});
     }
 }
