7f21936a70bfb3fc4206b4dae5364afe1c36dfad
Jaime <jmgo@gft.com>
2026-05-14 16:26:59 +0200

    [reporting] fix(config): add h2 database and override properties to allow CI tests to run

--- src/test/java/org/example/reportsworskhopgft/eventlog/infrastructure/config/OpenApiIntegrationTest.java
+++ src/test/java/org/example/reportsworskhopgft/eventlog/infrastructure/config/OpenApiIntegrationTest.java
@@ -1,9 +1,12 @@
+
+
 package org.example.reportsworskhopgft.eventlog.infrastructure.config;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -12,6 +15,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb",
+        "spring.datasource.driverClassName=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "spring.liquibase.enabled=false"
+})
 class OpenApiIntegrationTest {
 
     @Autowired
@@ -21,7 +32,7 @@ class OpenApiIntegrationTest {
     void should_generate_openapi_specification_with_schemas() throws Exception {
 
         mockMvc.perform(get("/v3/api-docs"))
-                .andExpect(status().isOk()) // Que devuelva 200 OK
+                .andExpect(status().isOk())
                 .andExpect(jsonPath("$.info.title").value("Reporting API"))
                 .andExpect(jsonPath("$.components.schemas.SystemStatsProjection").exists())
                 .andExpect(jsonPath("$.components.schemas.OrderHistoryProjection").exists());
